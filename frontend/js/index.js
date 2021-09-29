a9d7a9f3b55bf6d4238005cdbfabc2e7e6d7d3fc
Mael Tissot <mail.tissot.mael@gmail.com>
2021-09-29 09:05:02 +0200

    update index design

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,17 +1,32 @@
-fetch("https://api.orinoco.maeltissot.com/api/cameras")
-            .then ((response) => response.json())
-            .then ((array) => { buildProducts(array) })
+let urlAPI = (location.hostname === "localhost" || location.hostname === "127.0.0.1" || !location.hostname) ? "http://localhost:3000" : "https://api.orinoco.maeltissot.com"
+
+fetch(urlAPI + "/api/cameras")
+    .then((response) => {
+        if (response.ok) {
+            response.json()
+                .then((array) => { buildProducts(array) })
+        }
+        else {
+            console.log('Mauvaise réponse du réseau');
+        }
+    })
+    .catch((error) => {
+        console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
+        document.getElementById('productContainer').innerHTML = "déso"
+    });
 
 
-            function buildProducts(data) {
-                data.forEach((item) => { 
-                    // Formate le prix du produit 
-                    let formatedPrice = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(item.price / 100);
-                    // Construit la carte produit de chaque objet du tableau
-                    document.getElementById('productContainer').innerHTML += `
+function buildProducts(data) {
+    data.forEach((item) => {
+        // Formate le prix du produit 
+        let formatedPrice = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(item.price / 100);
+        // Construit la carte produit de chaque objet du tableau
+        document.getElementById('productContainer').innerHTML += `
                     <div class="col-12 col-md-6 col-xl-4 p-3">
                         <div class="card text-center">
-                            <img src="${item.imageUrl}" class="card-img-top" alt="Appareil photo ${item.name}">
+                            <div class="overflow-hidden">
+                                <img src="${item.imageUrl}" class="card-img-top" alt="Appareil photo ${item.name}">
+                            </div>
                             <div class="card-body">
                                 <h5 class="card-title">${item.name}</h5>
                                 <p class="card-text">${item.description}</p>
@@ -19,5 +34,5 @@ fetch("https://api.orinoco.maeltissot.com/api/cameras")
                             </div>
                         </div>
                     </div>`
-                 })
-            }
+    })
+}
