13a107ea0a0bd4c816b8d0403fed9ffdf430ec57
Mael Tissot <mail.tissot.mael@gmail.com>
2021-09-29 13:49:44 +0200

    update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -7,28 +7,29 @@ fetch(urlAPI + "/api/cameras")
                 .then((array) => { buildProducts(array) })
         }
         else {
-            console.log('Mauvaise réponse du réseau');
+            console.log('Réponse fetch incorrecte');
         }
     })
     .catch((error) => {
-        console.log('Il y a eu un problème avec l\'opération fetch: ' + error.message);
-        document.getElementById('productContainer').innerHTML = "déso"
+        console.log('Erreur lors du fetch : ' + error.message);
+        document.getElementById('productContainer').innerHTML = '<p class="display-6 fs-4 text-center text-danger">Une erreur est survenue, veuillez rééssayer ultérieurement.</p>'
     });
 
 
 function buildProducts(data) {
+    document.getElementById('productContainer').innerHTML = ""
     data.forEach((item) => {
         // Formate le prix du produit 
         let formatedPrice = new Intl.NumberFormat('fr-FR', { style: 'currency', currency: 'EUR' }).format(item.price / 100);
         // Construit la carte produit de chaque objet du tableau
         document.getElementById('productContainer').innerHTML += `
                     <div class="col-12 col-md-6 col-xl-4 p-3">
-                        <div class="card text-center">
+                        <div class="card text-center shadow-sm">
                             <div class="overflow-hidden">
                                 <img src="${item.imageUrl}" class="card-img-top" alt="Appareil photo ${item.name}">
                             </div>
                             <div class="card-body">
-                                <h5 class="card-title">${item.name}</h5>
+                                <h3 class="card-title">${item.name}</h3>
                                 <p class="card-text">${item.description}</p>
                                 <a href="/produit.html?id=${item._id}" class="btn btn-primary"><i class="fas fa-shopping-cart"></i> À partir de ${formatedPrice}</a>
                             </div>
